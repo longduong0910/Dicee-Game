9f2a114c9a319b4f88ed510b1e7ecb6515210a9e
longduong0910 <121540740+longduong0910@users.noreply.github.com>
2024-11-03 02:07:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
 
 var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
 
-var randomImageSource1 = "images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
-var randomImageSource2 = "images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
